456f14f972672f10ce1d97ebf30799802f3236e1
Jinoh Kim <juju08217@daum.net>
2022-02-26 21:23:00 +0900

    5장. 대강 통과하는 테스트 작성

--- functional_test.py
+++ functional_test.py
@@ -28,24 +28,26 @@ class NewVisitorTest(unittest.TestCase):
 
         # "공작깃털 사기"라고 텍스트 상자에 입력
         inputbox.send_keys('공작깃털 사기')
-
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에 "1: 공작깃털 사기" 아이템이 추가됨.
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: 공작깃털 사기' for row in rows),
-            '신규 작업이 테이블에 표시되지 않는다'
-        )
-
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다
-        self.fail('Finish the test')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
+        inputbox.send_keys(Keys.ENTER)
 
         # 페이지는 다시 갱신되고, 두 개의 아이템이 목록에 보인다
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        self.assertIn('2: 공작깃털을 이용해서 그물 만들기', [row.text for row in rows])
 
-        # assert 'Django' in 다rowser.title  # 현재의 스타팅 페이지 타이틀에는 Django가 들어있지 않아 아래와 같이 테스트를 진행함.
+        self.fail('Finish the test')
+
+
+
+        # assert 'Django' in browser.title  # 현재의 스타팅 페이지 타이틀에는 Django가 들어있지 않아 아래와 같이 테스트를 진행함.
         # assert 'congratulation' in browser.title.lower(), f"Browser title was {browser.title}"
 
 
--- list/tests.py
+++ list/tests.py
@@ -10,8 +10,25 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     # 뷰를 위한 테스트를 작성할 때는 단순히 빈 함수를 작성하는 것이 아니라 HTML 형식의 실제 응답을 반환하는 함수를 작성해야 한다.
+    # TODO 이 테스트는 문제가 있다... 나중에 고치던가 해야함.
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html', request=request)
+        print(response.content.decode())
+        print(expected_html)
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html', {'new_item_text': '신규 작업 아이템'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
+
