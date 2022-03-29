5ba12d89e45dcee2e02da99fbbadf9e8dccb604a
Jinoh Kim <juju08217@daum.net>
2022-03-29 09:34:49 +0900

    11장 작업 아이템용 신규 폼

---
+++ list/tests/test_forms.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from list.forms import ItemForm, EMPTY_LIST_ERROR
+
+class ItemformTest(TestCase):
+    def test_form_renders_item_text_input(self):
+        form = ItemForm()
+        self.assertIn('placeholder="작업 아이템 입력"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+        # self.fail(form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
